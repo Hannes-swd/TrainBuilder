25548311082266aa637c52069f4161a419b74426
Hannes <hannes-swd@users.noreply.github.com>
2026-02-22 17:51:27 +0100

    mauszeiger endern

--- TrainBuilder/mauszeiger.cpp
+++ TrainBuilder/mauszeiger.cpp
@@ -3,5 +3,10 @@
 #include <iostream>
 
 void mauszeiger(std::string mouszeiger) {
-
+	if (mouszeiger == "Standard")
+		SetMouseCursor(MOUSE_CURSOR_DEFAULT);
+	if (mouszeiger == "Pfeil")
+		SetMouseCursor(MOUSE_CURSOR_ARROW);
+	if (mouszeiger == "Fadenkreuz")
+		SetMouseCursor(MOUSE_CURSOR_CROSSHAIR);
 }
